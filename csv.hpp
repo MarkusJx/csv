3bc408aee802a52c7c4567a8fc847e45ae7f9481
Markus <28785953+markusjx@users.noreply.github.com>
2021-03-24 13:05:28 +0100

    Switch gtest name on windows
    Signed-off-by: Markus <28785953+MarkusJx@users.noreply.github.com>

--- csv.hpp
+++ csv.hpp
@@ -54,12 +54,10 @@
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
 #   define CSV_WINDOWS
 #   undef CSV_UNIX
-#pragma message("Building on windows")
 #elif defined(__LINUX__) || defined(__APPLE__) || defined (__CYGWIN__) || defined(__linux__) || defined(__FreeBSD__) || \
         defined(unix) || defined(__unix) || defined(__unix__)
 #   define CSV_UNIX
 #   undef CSV_WINDOWS
-#pragma message("Building on unix")
 #endif
 
 #ifndef CSV_NODISCARD
